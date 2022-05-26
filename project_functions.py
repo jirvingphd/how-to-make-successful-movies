d57b7d27222148fbf211749d4bb59dfb163d7185
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2022-05-26 13:56:19 -0400

    renamed model insights to specify its regression models

--- project_functions.py
+++ project_functions.py
@@ -235,3 +235,50 @@ def find_outliers_IQR(data, verbose=True):
 		print(f"- {outliers.sum()} outliers found in {data.name} using IQR.")
 		
 	return outliers
+	
+	
+	
+	
+def read_and_fix_json(JSON_FILE):
+    """Attempts to read in json file of records and fixes the final character
+    to end with a ] if it errors.
+    
+    Args:
+        JSON_FILE (str): filepath of JSON file
+        
+    Returns:
+        DataFrame: the corrected data from the bad json file
+    """
+    try: 
+        previous_df =  pd.read_json(JSON_FILE)
+    
+    ## If read_json throws an error
+    except:
+        
+        ## manually open the json file
+        with open(JSON_FILE,'r+') as f:
+            ## Read in the file as a STRING
+            bad_json = f.read()
+            
+            ## if the final character doesn't match first, select the right bracket
+            first_char = bad_json[0]
+            final_brackets = {'[':']', 
+                           "{":"}"}
+            ## Select expected final brakcet
+            final_char = final_brackets[first_char]
+            
+            ## if the last character in file doen't match the first char, add it
+            if bad_json[-1] != final_char:
+                good_json = bad_json[:-1]
+                good_json+=final_char
+            else:
+                raise Exception('ERROR is not due to mismatched final bracket.')
+            
+            ## Rewind to start of file and write new good_json to disk
+            f.seek(0)
+            f.write(good_json)
+           
+        ## Load the json file again now that its fixed
+        previous_df =  pd.read_json(JSON_FILE)
+        
+    return previous_df
