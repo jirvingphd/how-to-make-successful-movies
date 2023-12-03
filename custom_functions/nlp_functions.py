ff931056ae0ae2b0f2fd1f3db4d80931617fd8ea
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2023-12-02 19:11:08 -0500

    Updated NLP notebooks WIP

--- custom_functions/nlp_functions.py
+++ custom_functions/nlp_functions.py
@@ -240,7 +240,7 @@ def custom_preprocess_text(
 	
 		## Determine final form of output list of tokens/lemmas
 		if use_lemmas:
-			tokens.append(token.lemma_)
+			tokens.append(token.lemma_.lower())
 		elif lowercase==True:
 			tokens.append(token.text.lower())
 		else: 
@@ -251,59 +251,59 @@ def custom_preprocess_text(
 
 
 
-# def batch_preprocess_texts(
-# 	texts,
-# 	nlp=None,
-# 	remove_stopwords=True,
-# 	remove_punct=True,
-# 	use_lemmas=False,
-# 	disable=["ner"],
-# 	batch_size=50,
-# 	n_process=-1,
-# ):
-# 	"""Efficiently preprocess a collection of texts using nlp.pipe()
+def batch_preprocess_texts(
+	texts,
+	nlp=None,
+	remove_stopwords=True,
+	remove_punct=True,
+	use_lemmas=False,
+	disable=["ner"],
+	batch_size=50,
+	n_process=-1,
+):
+	"""Efficiently preprocess a collection of texts using nlp.pipe()
 
-# 	Args:
-# 		texts (collection of strings): collection of texts to process (e.g. df['text'])
-# 		nlp (spacy pipe), optional): Spacy nlp pipe. Defaults to None; if None, it creates a default 'en_core_web_sm' pipe.
-# 		remove_stopwords (bool, optional): Controls stopword removal. Defaults to True.
-# 		remove_punct (bool, optional): Controls punctuation removal. Defaults to True.
-# 		use_lemmas (bool, optional): lemmatize tokens. Defaults to False.
-# 		disable (list of strings, optional): named pipeline elements to disable. Defaults to ["ner"]: Used with nlp.pipe(disable=disable)
-# 		batch_size (int, optional): Number of texts to process in a batch. Defaults to 50.
-# 		n_process (int, optional): Number of CPU processors to use. Defaults to -1 (meaning all CPU cores).
+	Args:
+		texts (collection of strings): collection of texts to process (e.g. df['text'])
+		nlp (spacy pipe), optional): Spacy nlp pipe. Defaults to None; if None, it creates a default 'en_core_web_sm' pipe.
+		remove_stopwords (bool, optional): Controls stopword removal. Defaults to True.
+		remove_punct (bool, optional): Controls punctuation removal. Defaults to True.
+		use_lemmas (bool, optional): lemmatize tokens. Defaults to False.
+		disable (list of strings, optional): named pipeline elements to disable. Defaults to ["ner"]: Used with nlp.pipe(disable=disable)
+		batch_size (int, optional): Number of texts to process in a batch. Defaults to 50.
+		n_process (int, optional): Number of CPU processors to use. Defaults to -1 (meaning all CPU cores).
 
-# 	Returns:
-# 		list of tokens
-# 	"""
-# 	# from tqdm.notebook import tqdm
-# 	from tqdm import tqdm
+	Returns:
+		list of tokens
+	"""
+	# from tqdm.notebook import tqdm
+	from tqdm import tqdm
 
-# 	if nlp is None:
-# 		nlp = spacy.load("en_core_web_sm")
+	if nlp is None:
+		nlp = spacy.load("en_core_web_sm")
 
-# 	processed_texts = []
+	processed_texts = []
 
-# 	for doc in tqdm(nlp.pipe(texts, disable=disable, batch_size=batch_size, n_process=n_process)):
-# 		tokens = []
-# 		for token in doc:
-# 			# Check if should remove stopwords and if token is stopword
-# 			if (remove_stopwords == True) and (token.is_stop == True):
-# 				# Continue the loop with the next token
-# 				continue
+	for doc in tqdm(nlp.pipe(texts, disable=disable, batch_size=batch_size, n_process=n_process)):
+		tokens = []
+		for token in doc:
+			# Check if should remove stopwords and if token is stopword
+			if (remove_stopwords == True) and (token.is_stop == True):
+				# Continue the loop with the next token
+				continue
 
-# 			# Check if should remove stopwords and if token is stopword
-# 			if (remove_punct == True) and (token.is_punct == True):
-# 				continue
+			# Check if should remove stopwords and if token is stopword
+			if (remove_punct == True) and (token.is_punct == True):
+				continue
 
-# 			## Determine final form of output list of tokens/lemmas
-# 			if use_lemmas:
-# 				tokens.append(token.lemma_)
-# 			else:
-# 				tokens.append(token.text.lower())
+			## Determine final form of output list of tokens/lemmas
+			if use_lemmas:
+				tokens.append(token.lemma_)
+			else:
+				tokens.append(token.text.lower())
 
-# 		processed_texts.append(tokens)
-# 	return processed_texts
+		processed_texts.append(tokens)
+	return processed_texts
 
 
 
